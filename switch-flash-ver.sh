b0c375f80d30634f593561e4f3db98e9a30623a2
Richard Dubný <72037908+R1xhark@users.noreply.github.com>
2023-11-10 15:00:46 +0100

    Update switch-flash-ver.sh
    used wrong logic, error handling didnt work, if statement for screen -ls check catched every SN

--- switch-flash-ver.sh
+++ switch-flash-ver.sh
@@ -1,61 +1,58 @@
 #!/bin/bash
 
-echo "Switch Flash script"
-echo "Input 'k' to exit"
-
-while true; do
-    read p "Enter Switch SN: " sn
-    read -p "Enter Switch IP: " ip
-    
-    is_unit_up(){
+is_unit_up() {
     ping -c 1 "$1" &> /dev/null
     return $?
 }
 
-if screen -ls | grep -q ".*$sn*."; then
-    echo "Switch Flash already in progress."
-else
-    if is_unit_up "ip"; then
+while true; do
+    echo "Switch Flash script"
+    echo "Input 'k' to exit"
 
-    if [[ "$ip" == 'k' || "$sn" == 'k' ]]; then
-        echo "Exiting the script."
+    read -p "Enter Switch SN:" sn
+    read -p "Enter Switch IP:" ip
+
+    if [[ "$sn" == 'k' || "$ip" == 'k' || "$sn" == 'K' || "$sn" == 'K' ]]; then
         exit 0
     fi
 
-    check=$(ssh "$ip" cat /etc/lsb-release)
+    if screen -ls | grep -q "*.$sn.*"; then
+        echo "Switch Flash already in progress."
+    else
+        if is_unit_up "$ip" && [ -n "$sn" ] && [ -n "$ip" ]; then
+            check=$(ssh "$ip" cat /etc/lsb-release)
 
-    if [[ -n "$sn" && -n "$ip" ]]; then
-        if [[ "$sn" == *"8260"* ]]; then
-            if echo "$check" | grep -q '^DISTRIB_RELEASE=2017\.11\.020\.amzn$'; then
-                echo "Switch is already flashed."
+            if [[ "$sn" == *"8260"* ]]; then
+                if echo "$check" | grep -q '^DISTRIB_RELEASE=2017\.11\.020\.amzn$'; then
+                    echo "Switch is already flashed."
+                else
+                    echo "Flashing CS8260-32X-DC-11 switch"
+                    screen -dmS "$sn" bash -c "bash /home/C2111004/remote_switch_flash_c8260.sh $ip"
+                fi
+            elif [[ "$sn" == *"8060"* ]]; then
+                if echo "$check" | grep -q '^DISTRIB_RELEASE=2017\.11\.09$'; then
+                    echo "Switch is already flashed."
+                else
+                    echo "Flashing AS8060-32X-DC-11 switch"
+                    screen -dmS "$sn" bash -c "bash /home/C2111004/remote_switch_flash_as.sh $ip"
+                fi
+            elif [[ "$sn" == *"8000"* ]]; then
+                if echo "$check" | grep -q '^DISTRIB_RELEASE=2017\.11\.151$'; then
+                    echo "Switch is already flashed."
+                else
+                    echo "Flashing CS8000-32X-DC-11 switch"
+                    screen -dmS "$sn" bash -c "bash /home/C2111004/remote_switch_flash_cs.sh $ip"
+                fi
             else
-                echo "Flashing CS8260-32X-DC-11 switch."
-                screen -dmS "$sn" bash -c "bash /home/C2111004/remote_switch_flash_c8260.sh $ip"
+                echo "Error: SN $sn not valid."
             fi
-        elif [[ "$sn" == *"8060"* ]]; then
-            if echo "$check" | grep -q '^DISTRIB_RELEASE=2017\.11\.09$'; then
-                echo "Switch is already flashed."
-            else
-                echo "Flashing AS8060-32X-DC-11 switch."
-                screen -dmS "$sn" bash -c "bash /home/C2111004/remote_switch_flash_as.sh $ip"
-            fi
-        elif [[ "$sn" == *"8000"* ]]; then
-            if echo "$check" | grep -q '^DISTRIB_RELEASE=2017\.11\.151$'; then
-                echo "Switch is already flashed."
-            else
-                echo "Flashing CS8000-32X-DC-11 switch."
-                screen -dmS "$sn" bash -c "bash /home/C2111004/remote_switch_flash_cs.sh $ip"
+
+            read -p "Do you want to enter another IP? (Y/N):" choice
+            if [[ "$choice" != "Y" && "$choice" != "y" ]]; then
+                exit 0
             fi
         else
-            echo "Error: Invalid SN: $sn or IP: $ip."
+            echo "Unit is offline or SN/IP input is missing."
         fi
-    else
-        echo "Both IP and SN must be provided."
-    fi
-
-    read -p "Do you want to enter another switch? (Y/N): " choice
-    if [[ "$choice" != "Y" && "$choice" != "y" ]]; then
-        echo "Exiting the script."
-        exit 0
     fi
 done
